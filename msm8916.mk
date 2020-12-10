269cd8fe3c9e8801bd664616a64c612f00f55c19
palmbeach05 <davmcleod87@gmail.com>
2020-12-10 07:21:40 -0500

    msm8916-common: Add tzdata and update fstabs
    This helps to resolve missing tzdata errors and mounting issues

--- msm8916.mk
+++ msm8916.mk
@@ -449,6 +449,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
+# Tzdata
+PRODUCT_PACKAGES += \
+    tzdata_twrp
+    
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
